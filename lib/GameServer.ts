ec4471bfc9af25e3d64c6c43a380d34e4c416323
Juan Camilo Guarín Peñaranda <jcguarinpenaranda@gmail.com>
2016-10-20 09:46:04 -0500

    implemented events

--- lib/GameServer.ts
+++ lib/GameServer.ts
@@ -1,7 +1,12 @@
 import * as socket from 'socket.io';
 
-export interface GameServerParams{
-    io:any
+export interface GameServerParams {
+    io:any,
+    maxAllowed?:number
+}
+
+export interface GameServerEventParams {
+    eventName:string
 }
 
 export class GameServer{
@@ -11,10 +16,15 @@ export class GameServer{
 
     constructor(params:GameServerParams){
         this.io = params.io;
+        this.maxAllowed = params.maxAllowed || 30;
 
         this.io.sockets.on('connection', (socket:any)=>{
             this.onNewConnection(socket);
-
+            
+            // se asignan los eventos del juego
+            this.onGameEvents(socket);
+            
+            // evento por defecto para la desconexión
             socket.on('disconnect', ()=>{
                 this.onDisconnection(socket);
             })
@@ -22,7 +32,19 @@ export class GameServer{
 
     }
 
+    onGameEvents(socket:any):void{
+        // función básica para que se mueva el jugador
+        socket.on('userEvent', function(data:GameServerEventParams){
+            socket.broadcast.emit(data.eventName,data);
+        })
+    }
+
     onNewConnection(socket:any):void{
+        // se respeta la cantidad de usuarios máximos permitidos
+        if( (this.clients.length + 1) > this.maxAllowed) {
+            return;
+        }
+
         console.log("connected "+socket.id);
         
         // se agrega el socket a la lista de clientes actuales
@@ -30,10 +52,22 @@ export class GameServer{
 
         // se emite que llegó alguien nuevo
         this.broadcastPresence(socket);
+
+        // otros eventos por defecto
+        this.emitMe(socket);
+        this.emmitConnectedClients(socket);
     }
 
     onDisconnection(socket:any){
         console.log("disconnected "+socket.id);
+        
+        // se emite a todos los otros que alguien se desconectó
+        socket.broadcast.emit('disconnected',socket.id);
+        
+        // se quita el cliente que se desconectó
+        this.clients = this.clients.filter(function(actual){
+            return actual.id !== socket.id;
+        })
     }
 
     broadcastPresence(socket:any){
@@ -41,11 +75,19 @@ export class GameServer{
     }
 
     emmitConnectedClients(socket:any){
-
+        let others = this.clients.filter(function(actual){
+            return actual.id !== socket.id;
+        })
+    
+        others = others.map(function(client){
+            return client.id;
+        });
+    
+        socket.emit('connected_clients',others);
     }
 
     emitMe(socket:any){
-
+        socket.emit('me', socket.id);
     }
  
 }
